1d3667dcd1a5a5e415e3cb3ad645ea8e9dc4d84c
Flyhiawa <flyhiawa@gmail.com>
2025-11-18 01:02:54 -0300

    Project in Python

---
+++ FAVhypercubes.py
@@ -0,0 +1,20 @@
+def Faces(d):
+    if d < 0:
+        print("The number of dimensions can't go below 0!")
+        return None
+    calc = d * (d - 1) / 2 * 2 ** (d - 2)
+    return calc
+
+def Edges(d):
+    if d < 0:
+        print("The number of dimensions can't go below 0!")
+        return None
+    calc = d * 2 ** (d - 1)
+    return calc
+
+def Vertices(d):
+    if d < 0:
+        print("The number of dimensions can't go below 0!")
+        return None
+    calc = 2 ** d
+    return calc
